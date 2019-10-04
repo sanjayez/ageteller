0439c79dde48f465bbbecd1e80de18de582043d9
sanjay <sanu2336@gmail.com>
2019-10-04 08:40:50 -0700

    Updates

---
+++ precache-manifest.e5c115d2ab997d82a6d3a1bd8a4b81a3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad6fa0f233ad232c770a3f47109aeee6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fef4e2cdd5e93733c21a",
+    "url": "/static/css/main.dbc34170.chunk.css"
+  },
+  {
+    "revision": "1e397df061a154c12ea1",
+    "url": "/static/js/2.0389230d.chunk.js"
+  },
+  {
+    "revision": "fef4e2cdd5e93733c21a",
+    "url": "/static/js/main.80096b69.chunk.js"
+  },
+  {
+    "revision": "3416640f62a2707d5bc2",
+    "url": "/static/js/runtime-main.dc742d79.js"
+  },
+  {
+    "revision": "4580939b3bb7bd28d9dc34bb0e395f4c",
+    "url": "/static/media/party-popper.4580939b.jpg"
+  }
+]);
